def003cf6e3a2ea443ff640c9693161934d66093
vrknetha <vrknetha@gmail.com>
2024-12-08 01:25:43 +0530

    fixes ci command

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,45 +1,34 @@
-import { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
+export default defineConfig({
   testDir: './tests',
-  timeout: 30000,
-  expect: {
-    timeout: 5000
-  },
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
-    ['html'],
-    ['list']
-  ],
+  reporter: 'html',
   use: {
-    actionTimeout: 0,
+    baseURL: 'http://localhost:8080',
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure'
   },
-  outputDir: 'test-results/',
+  webServer: {
+    command: 'npm run serve',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+    timeout: 5000,
+  },
   projects: [
     {
       name: 'chromium',
-      use: {
-        browserName: 'chromium',
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
     {
       name: 'firefox',
-      use: {
-        browserName: 'firefox',
-      },
+      use: { ...devices['Desktop Firefox'] },
     },
     {
       name: 'webkit',
-      use: {
-        browserName: 'webkit',
-      },
+      use: { ...devices['Desktop Safari'] },
     },
   ],
-};
-
-export default config; 
+}); 
